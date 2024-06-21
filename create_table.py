da1588e3f3b1c4ac01c874e891a94e8928d97974
JaeHeon-Kim <jaeheon998795@gmail.com>
2024-06-21 17:37:43 +0900

    mission complete

--- create_table.py
+++ create_table.py
@@ -9,7 +9,9 @@ file_root = "C:\\Users\\pjy28\\Desktop\\develop"
 root = Tk()  # 이미지선택창 열기
 image_list = askopenfilenames()
 root.destroy()  # 이미지선택창 닫기
-BASE_DIR = image_list[0]  # 이미지리스트에서 경로 추출
+
+
+# BASE_DIR = image_list[0]  # 이미지리스트에서 경로 추출
 
 def extract_numbers(image_list):
     extracted_numbers = []
@@ -27,19 +29,12 @@ name_number = extract_numbers(image_list)
 sorted_numbers = sorted(name_number) # 숫자 오름차순
 
 
-# %% 표_리스트 만들기
-
-# 표_리스트 = list(set([i.split("_")[0][:-1] for i in image_list]))
-# 표_리스트.sort()
-
-# print(표_리스트)
-
+######################## 한글 시작 ##################################
 hwp=win32.Dispatch("HWPFrame.HwpObject")
 
 hwp.RegisterModule("FilePathCheckDLL", "SecurityModule")
 hwp.XHwpWindows.Item(0).Visible = True
 
-# hwp.Open("C:\\Users\\pjy28\\Desktop\\develop\\picture_test.hwpx")
 
 # 가운데 정렬
 def align_center():
@@ -63,31 +58,27 @@ def set_margin(left, right, top, bottom, header, footer) :
     hwp.HParameterSet.HSecDef.HSet.SetItem("ApplyTo", 3)  # 문서 전체 변경
     hwp.HAction.Execute("PageSetup", hwp.HParameterSet.HSecDef.HSet)
 
-def create_table(row, column, width, height):
+def create_table(row, column, table_width, table_height, image_url, image_idx, image_width, image_height): ##(줄 갯수, 칸 갯수, 사진 너비, 사진 높이, 사진 주소, 사진 번호)
     hwp.HAction.GetDefault("TableCreate", hwp.HParameterSet.HTableCreation.HSet)
     hwp.HParameterSet.HTableCreation.Rows = row
     hwp.HParameterSet.HTableCreation.Cols = column
     hwp.HParameterSet.HTableCreation.WidthType = 2
     hwp.HParameterSet.HTableCreation.HeightType = 1
-    hwp.HParameterSet.HTableCreation.WidthValue = hwp.MiliToHwpUnit(width)
-    hwp.HParameterSet.HTableCreation.HeightValue = hwp.MiliToHwpUnit(height)
+    hwp.HParameterSet.HTableCreation.WidthValue = hwp.MiliToHwpUnit(table_width)
+    hwp.HParameterSet.HTableCreation.HeightValue = hwp.MiliToHwpUnit(table_height)
     hwp.HParameterSet.HTableCreation.CreateItemArray("ColWidth", column)
-    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(0, hwp.MiliToHwpUnit(47))
-    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(1, hwp.MiliToHwpUnit(25))
-    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(2, hwp.MiliToHwpUnit(33))
-    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(3, hwp.MiliToHwpUnit(42))
+    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(0, hwp.MiliToHwpUnit(47)) ## 첫째줄 너비
+    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(1, hwp.MiliToHwpUnit(25)) ## 둘째줄 너비
+    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(2, hwp.MiliToHwpUnit(33)) ## 셋째줄 너비
+    hwp.HParameterSet.HTableCreation.ColWidth.SetItem(3, hwp.MiliToHwpUnit(42)) ## 넷째줄 너비
 
     hwp.HParameterSet.HTableCreation.CreateItemArray("RowHeight", row)
-    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(0, hwp.MiliToHwpUnit(8))
-    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(1, hwp.MiliToHwpUnit(83))
-    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(2, hwp.MiliToHwpUnit(8))
-    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(3, hwp.MiliToHwpUnit(11))
-    # for i in range(column):
-    #     hwp.HParameterSet.HTableCreation.ColWidth.item[i] = hwp.MiliToHwpUnit((width / column) - 3.6)
-    # hwp.HParameterSet.HTableCreation.CreateItemArray("RowHeight", row)
-    # for i in range(row):
-    #     hwp.HParameterSet.HTableCreation.RowHeight.item[i] = hwp.MiliToHwpUnit((height / row) - 1.0)
-    hwp.HParameterSet.HTableCreation.TableProperties.Width = mm_to_hu(width)
+    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(0, hwp.MiliToHwpUnit(8)) ## 제목 높이
+    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(1, hwp.MiliToHwpUnit(83)) ## 사진 높이
+    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(2, hwp.MiliToHwpUnit(8)) ## 개요 높이
+    hwp.HParameterSet.HTableCreation.RowHeight.SetItem(3, hwp.MiliToHwpUnit(10)) ## 내용 높이
+
+    hwp.HParameterSet.HTableCreation.TableProperties.Width = mm_to_hu(table_width)
     hwp.HParameterSet.HTableCreation.TableProperties.OutsideMarginLeft = hwp.MiliToHwpUnit(0)
     hwp.HParameterSet.HTableCreation.TableProperties.OutsideMarginRight = hwp.MiliToHwpUnit(0)
     hwp.HParameterSet.HTableCreation.TableProperties.OutsideMarginTop = hwp.MiliToHwpUnit(0)
@@ -99,7 +90,7 @@ def create_table(row, column, width, height):
     hwp.HAction.Run("TableCellBlockRow")  # 표 1행 선택
     hwp.HAction.Run("TableMergeCell")
     hwp.HAction.GetDefault("InsertText", hwp.HParameterSet.HInsertText.HSet)
-    hwp.HParameterSet.HInsertText.Text = "상록초등학교 1동교사 및 순성초등학교 1동교사 정밀안전점검 용역_순성초등학교 1동교사"
+    hwp.HParameterSet.HInsertText.Text = "상록초등학교 1동교사 및 순성초등학교 1동교사 정밀안전점검 용역_순성초등학교 1동교사" ## 표 제목
     hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet);
     hwp.HAction.Run("TableCellBlockRow")
     hwp.HAction.Run("ParagraphShapeAlignCenter")
@@ -140,21 +131,17 @@ def create_table(row, column, width, height):
     hwp.HAction.Run("MoveUp")
     hwp.HAction.Run("MoveUp")
 
-    for idx, content in enumerate(sorted_numbers):
-         
-         hwp.HAction.GetDefault("TablePropertyDialog", hwp.HParameterSet.HShapeObject.HSet)
-         hwp.HParameterSet.HShapeObject.ShapeTableCell.Editable = 1
-         hwp.HParameterSet.HShapeObject.ShapeTableCell.CellCtrlData.name = f"{idx}"
-    # InsertPicture(path, embeded, sizeoption, reverse, watermark,effect, width, height, callback)  
-    hwp.InsertPicture(BASE_DIR, True, 3, False, False, 0, 82, 82) 
+
+    ## 표 안에 이미지 넣기
+    insert_image(image_width, image_height, image_url, image_idx)
+
 
     align_center()
-    
     hwp.HAction.Run("MoveDown")
-
     hwp.HAction.Run("TableColBegin")  # 열 맨 앞으로 이동
 
-    insert_text('NO. 1')
+    ## 개요 작성
+    insert_text(f'NO. {image_idx + 1}')
     hwp.HAction.Run("MoveRight")
     insert_text('부재명')
     hwp.HAction.Run("MoveRight")
@@ -166,6 +153,32 @@ def create_table(row, column, width, height):
     align_center()
     hwp.HAction.Run("CharShapeBold")
 
+    ##표안에 셀 색 변경하기
+    act = hwp.CreateAction("CellFill")
+    createSet = act.CreateSet()
+    act.GetDefault(createSet)
+    fillAttrSet = createSet.CreateItemSet("FillAttr", "DrawFillAttr")
+
+    fillAttrSet.SetItem("Type", 1);
+    fillAttrSet.SetItem("WinBrushFaceStyle", 0xffffffff);
+    fillAttrSet.SetItem("WinBrushHatchColor", 0x00000000);
+    fillAttrSet.SetItem("WinBrushFaceColor", hwp.RGBColor(229, 229, 229)); ## 표 셀 색상
+    act.Execute(createSet);
+
+    ##표 테두리 두줄로 변경(아래)
+    hwp.HAction.GetDefault("CellBorder", hwp.HParameterSet.HCellBorderFill.HSet)
+    hwp.HParameterSet.HCellBorderFill.BorderTypeBottom = hwp.HwpLineType("DoubleSlim") ## Line Type 2중선
+    hwp.HParameterSet.HCellBorderFill.BorderWidthBottom = hwp.HwpLineWidth("0.7mm") ## 굵기 0.7mm 되나?
+    hwp.HAction.Execute("CellBorder", hwp.HParameterSet.HCellBorderFill.HSet)
+    hwp.HAction.Run("Cancel")
+
+    ##표 밖으로 나오기
+    hwp.HAction.Run("CloseEx")
+    hwp.HAction.Run("MoveLineEnd")
+    hwp.HAction.Run("BreakPara")
+
+
+
 def mm_to_hu(value):
     return hwp.MiliToHwpUnit(value)
 
@@ -174,6 +187,23 @@ def insert_text(text):
     hwp.HParameterSet.HInsertText.Text = text
     hwp.HAction.Execute("InsertText", hwp.HParameterSet.HInsertText.HSet)
 
+    ##사진 넣기
+def insert_image(image_width, image_height, image_url, image_idx):
+    hwp.HAction.GetDefault("TablePropertyDialog", hwp.HParameterSet.HShapeObject.HSet)
+    hwp.HParameterSet.HShapeObject.ShapeTableCell.Editable = 1
+    hwp.HParameterSet.HShapeObject.ShapeTableCell.CellCtrlData.name = f"{image_idx}"
+    hwp.InsertPicture(image_url, True, 3, False, False, 0, image_width, image_height) 
+
+
+
+
+
+
+####################실행####################################
+
 align_center()
 set_margin(15, 15, 20, 15, 15, 15)
-create_table(4, 4, 250, 210)
+
+for idx, image in enumerate(image_list):
+    image_url = image_list[idx] # 이미지리스트에서 경로 추출
+    create_table(4, 4, 250, 210, image_url, idx, 82, 82)
